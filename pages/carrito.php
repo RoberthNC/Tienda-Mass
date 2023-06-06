81dcf8f0539fdd04c6001be806ef53440665c8f4
Roberto Núñez <robertonc1599@gmail.com>
2023-06-06 14:27:34 -0500

    Logo de carrito modificado

--- pages/carrito.php
+++ pages/carrito.php
@@ -67,7 +67,7 @@
     <main class="main">
         <div class="seccion-carrito">
             <div class="contenedor-carrito">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="68" height="68" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="68" height="68" viewBox="0 0 24 24" stroke-width="1.5" stroke="#25318C" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
                     <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
