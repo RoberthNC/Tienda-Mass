d2e3deafd163370815267b7e19579beb64dc2d04
Roberto Núñez <robertonc1599@gmail.com>
2023-06-26 18:16:53 -0500

    Flecha añadida

--- pages/nuevoproveedor.php
+++ pages/nuevoproveedor.php
@@ -17,12 +17,14 @@
     </header>
     <main class="main">
         <div class="contenedor-izquierda">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-left" width="68" height="68" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M5 12l14 0" />
-                <path d="M5 12l6 6" />
-                <path d="M5 12l6 -6" />
-            </svg>
+            <a href="./admin.php">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-left" width="68" height="68" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M5 12l14 0" />
+                    <path d="M5 12l6 6" />
+                    <path d="M5 12l6 -6" />
+                </svg>
+            </a>
             <img src="../img/proveedores.jpg" alt="Nuevo proveedor">
         </div>
         <div class="contenedor-derecha">
